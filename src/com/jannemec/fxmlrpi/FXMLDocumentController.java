feab7791d5f931660c8124c5f61e882af67cbd02
u935 <u935@nemecw7.otk.cz>
2018-10-22 22:31:12 +0200

    Original repository

---
+++ src/com/jannemec/fxmlrpi/FXMLDocumentController.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jannemec.fxmlrpi;
+
+import com.jannemec.control.ClockControl;
+import com.jannemec.sensors.AM2321;
+import com.jannemec.sensors.BMP180;
+import com.jannemec.tools.MemCache;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.util.Duration;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author u935
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    private Timeline timeline = null;
+    protected SimpleDateFormat format = null;
+    protected com.jannemec.tools.MemCache mCache = null;
+    protected com.jannemec.sensors.AM2321 am2321 = null;
+    protected com.jannemec.sensors.BMP180 bmp180 = null;
+    
+    @FXML
+    private ClockControl clockControl;
+    @FXML
+    private Label tempLabel;
+    @FXML
+    private TextField tempTextField;
+    @FXML
+    private TextField temp2TextField;
+    @FXML
+    private Label humidLabel;
+    @FXML
+    private TextField humidTextField;
+    @FXML
+    private Label pressLabel;
+    @FXML
+    private TextField pressTextField;
+    @FXML
+    private TextField press2TextField;
+    @FXML
+    private TextField altitudeTextField;
+    
+    
+    @FXML
+    private javafx.scene.control.Button closeButton;
+    
+    @FXML
+    private void handleCloseButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        // get a handle to the stage
+        Stage stage = (Stage) closeButton.getScene().getWindow();
+        // do closing actions
+        stage.close();
+    }
+    
+    @FXML
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.mCache = new MemCache();
+        
+        this.tempTextField.setText("waiting for init");
+        this.format = new SimpleDateFormat("HH:mm:ss");
+        
+        timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        // Test hodiny
+        /*
+        timeline.getKeyFrames().add(
+            new KeyFrame(Duration.seconds(1), (ActionEvent event) -> { 
+                Date dt = new Date();
+                this.tempTextField.setText(this.format.format(dt));     
+            } // KeyFrame event handler
+        ));
+        timeline.playFromStart();
+        */
+        
+        // Temperature
+        this.am2321 = new AM2321(this.mCache);
+
+        this.bmp180 = new BMP180(mCache);
+        // Setup altitude based on position
+        this.bmp180.setSensorAltitude(350);  // For Jesenice, Central Bohemia, Czech Republic
+        // Output data to screen
+        /*for(int i = 0; i < 2; i++) {
+            System.out.printf("Humidity : %.2f prct %n", am2321.getHumidity());
+            System.out.printf("Temperature in Celsius : %.2f C %n", am2321.getTemperature());
+            Thread.sleep(2000);
+        }*/
+        try {
+            this.showValues();
+        } catch (Exception ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        System.out.println("App started!");
+        timeline.getKeyFrames().add(
+            new KeyFrame(Duration.seconds(30), (ActionEvent event) -> { 
+                try {     
+                    this.showValues();
+                } catch (Exception ex) {
+                    Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } // KeyFrame event handler
+        ));
+        timeline.playFromStart();
+    }
+
+    private void showValues() throws  Exception {
+        this.humidTextField.setText(String.format("%.0f", (double) this.am2321.getHumidity()));
+        this.tempTextField.setText(String.format("%.1f", (double) this.am2321.getTemperature()));
+        this.temp2TextField.setText(String.format("%.1f", (double) this.bmp180.getTemperature()));
+        this.pressTextField.setText(String.format("%.0f", (double) this.bmp180.getPressure()));
+        this.press2TextField.setText(String.format("%.0f", (double) this.bmp180.getPressureAtSeaLevel()));
+        this.altitudeTextField.setText(String.format("%.0f", (double) this.bmp180.getAltitude()));
+    }
+    
+}
---
+++ src/com/jannemec/fxmlrpi/RPiGPIOFX.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jannemec.fxmlrpi;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author u935
+ * java -cp ~/Dokumenty/rpi/RPiGPIOFX/RPiGPIOFX.jar com.jannemec.fxmlrpi.RPiGPIOFX
+ * java -cp RPiGPIOFX.jar com.jannemec.fxmlrpi.RPiGPIOFX
+ * java -Djava.ext.dirs=~/Dokumenty/rpi/armv6hf-sdk/rt/lib/ext -cp RPiGPIOFX.jar com.jannemec.fxmlrpi.RPiGPIOFX
+ * java -Djava.ext.dirs=~/Dokumenty/rpi/armv6hf-sdk/rt/lib/ext -jar RPiGPIOFX.jar
+ * gksu "java -jar RPiGPIOFX.jar"
+ */
+public class RPiGPIOFX extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) throws Exception {
+        launch(args);
+    }
+    
+}
