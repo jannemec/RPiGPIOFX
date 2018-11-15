992be21583b17db42c5d8e393795979667dbac67
u935 <u935@nemecw7.otk.cz>
2018-11-15 21:14:33 +0100

    Dbf, config

--- src/com/jannemec/fxmlrpi/FXMLDocumentController.java
+++ src/com/jannemec/fxmlrpi/FXMLDocumentController.java
@@ -19,14 +19,22 @@ import com.jannemec.sensors.TSL2561;
 import com.jannemec.sensors.HCSR04;
 import com.jannemec.sensors.RainSBX;
 import com.jannemec.sensors.Sensor;
+import com.jannemec.sensors.TCS3200;
 
 import com.jannemec.tools.MemCache;
 import com.jannemec.tools.ActionListener;
+import com.jannemec.tools.Tools;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -41,6 +49,24 @@ import javafx.scene.control.TextField;
 import javafx.util.Duration;
 import javafx.stage.Stage;
 import javafx.scene.control.CheckBox;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import com.pi4j.io.gpio.RaspiPin;
+import java.io.UnsupportedEncodingException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 /**
  *
@@ -60,7 +86,12 @@ public class FXMLDocumentController implements Initializable, ActionListener {
     protected com.jannemec.sensors.MotionPIR motionPIR = null;
     protected com.jannemec.sensors.HCSR04 hcsr04 = null;
     protected com.jannemec.sensors.Button button1 = null;
+    protected com.jannemec.sensors.TCS3200 tcs3200 = null;
+    protected com.jannemec.sensors.Button button2 = null;
     protected RPiCamera rPiCamera = null;
+    protected Session session = null;
+    protected Properties setup = null;
+    protected Tools tools = null;
     
     @FXML
     private ClockControl clockControl;
@@ -107,6 +138,12 @@ public class FXMLDocumentController implements Initializable, ActionListener {
     @FXML
     private TextField distanceTextField;
     
+    //@FXML
+    //private TextField colorTextField;
+    
+    //@FXML
+    //private TextField colorDtTextField;
+    
     @FXML
     private void handleCloseButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
@@ -119,12 +156,50 @@ public class FXMLDocumentController implements Initializable, ActionListener {
     @FXML
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        // read Properties
+        this.setup = new Properties();
+	try {
+            // load a properties file
+            InputStream in = Tools.getPropertyFile(FXMLDocumentController.class, "properties/setup.properties");
+            this.setup.load(in);          
+	} catch (IOException ex) {
+		//ex.printStackTrace();
+	} finally {
+		
+	}
         this.mCache = new MemCache();
+        this.mCache.setDelay(Integer.parseInt(setup.getProperty("general.refresh")) - 1);
         
+        
+        // Prepare date forated
         this.tempTextField.setText("waiting for init");
         this.format = new SimpleDateFormat("HH:mm:ss");
         this.formatWithDate = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
         this.formatCompact = new SimpleDateFormat("yyyyMMddHHmmss");
+        
+        //Establishing a session with required user details
+        this.session = Session.getInstance(this.setup, new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(setup.getProperty("mail.smtp.username"), setup.getProperty("mail.smtp.password"));
+            }
+        });
+        this.tools = new Tools();
+
+        try {
+            this.tools.dbfConnect(this.setup.getProperty("dbf.connectionString").replace("currentDir", this.tools.getJarPath()));
+            this.tools.dbfCreateTable();
+        } catch (SQLException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         timeline = new Timeline();
         timeline.setCycleCount(Timeline.INDEFINITE);
@@ -162,8 +237,13 @@ public class FXMLDocumentController implements Initializable, ActionListener {
         this.button1.setInteruptMode(true);
         this.button1.setActionListener(this);
         
+        this.tcs3200 = new TCS3200(mCache);
+        this.button2 = new Button(mCache, RaspiPin.GPIO_24);
+        this.button2.setInteruptMode(true);
+        this.button2.setActionListener(this);
+        
         try {
-            this.rPiCamera = new RPiCamera("/home/vlk/Dokumenty/rpi/RPiGPIOFX/photos");
+            this.rPiCamera = new RPiCamera(setup.getProperty("camera.dir", "/home/vlk/Dokumenty/rpi/RPiGPIOFX/photos"));
             this.rPiCamera
                 .setWidth(500).setHeight(500) // Set Camera to produce 500x500 images.
                 .setBrightness(75)                // Adjust Camera's brightness setting.
@@ -174,12 +254,7 @@ public class FXMLDocumentController implements Initializable, ActionListener {
 		.setEncoding(Encoding.JPG);
         } catch (FailedToRunRaspistillException e) {
         }
-        // create listener on button
-        
-        
-        
-        
-        
+
         try {
             this.showValues();
         } catch (Exception ex) {
@@ -187,7 +262,7 @@ public class FXMLDocumentController implements Initializable, ActionListener {
         }
         System.out.println("App started!");
         timeline.getKeyFrames().add(
-            new KeyFrame(Duration.seconds(30), (ActionEvent event) -> { 
+            new KeyFrame(Duration.seconds(Integer.parseInt(setup.getProperty("general.refresh"))), (ActionEvent event) -> { 
                 try {     
                     this.showValues();
                 } catch (Exception ex) {
@@ -219,6 +294,10 @@ public class FXMLDocumentController implements Initializable, ActionListener {
         } else {
             this.rainTextField.setText(this.format.format(this.rainSBX.getLastChangeDate()));
         };*/
+        
+        // And we have to store values to dbf
+        Date dt = new Date();
+        this.tools.storeDoubleValue(dt, "temperature", this.bmp180.getTemperature());
     }
 
     @Override
@@ -255,6 +334,12 @@ public class FXMLDocumentController implements Initializable, ActionListener {
         
         if (sensor.getClass().getName().equals("com.jannemec.sensors.Button")) {
             try {
+                if (this.button2.isOn()) {
+                    // Načtení barvy
+                    String color = this.tcs3200.getColor();
+                    //this.colorTextField.setText(color);
+                    //this.colorDtTextField.setText(this.formatWithDate.format(this.motionPIR.getLastChangeDate()));
+                }
                 if (this.button1.isOn()) {
                     // Take a picture
                     if (!(this.rPiCamera == null)) {
@@ -264,6 +349,36 @@ public class FXMLDocumentController implements Initializable, ActionListener {
                         File image = this.rPiCamera.takeStill(filename, 650, 650);
 			System.out.println("New PNG image saved to:\n\t" + image.getAbsolutePath());
                         //this.rPiCamera.takeStill(filename);
+                        
+                        // Send Mail with photo
+                        try {
+                            //Creating a Message object to set the email content
+                            MimeMessage msg = new MimeMessage(session);
+                            //Storing the comma seperated values to email addresses
+                            String to = "jannemec@centrum.cz";
+                            /*Parsing the String with defualt delimiter as a comma by marking the boolean as true and storing the email addresses in an array of InternetAddress objects*/
+                            InternetAddress[] address = InternetAddress.parse(to, true);
+                            //Setting the recepients from the address variable
+                            msg.setRecipients(Message.RecipientType.TO, address);
+                            String from = "jan.nemec@cscz.biz";
+                            msg.setFrom(from);
+                            String timeStamp = formatWithDate.format(new Date());
+                            msg.setSubject("Photo from RPi : " + timeStamp);
+                            msg.setSentDate(new Date());
+                            msg.setText("The photo taken from RPi - according to action");
+                            msg.setHeader("XPriority", "1");
+                            MimeBodyPart messageBodyPart = new MimeBodyPart();
+                            DataSource source = new FileDataSource(image.getAbsolutePath());
+                            messageBodyPart.setDataHandler(new DataHandler(source));
+                            messageBodyPart.setFileName(filename);
+                            Multipart multipart = new MimeMultipart();
+                            multipart.addBodyPart(messageBodyPart);
+                            msg.setContent(multipart);
+                            Transport.send(msg);
+                            System.out.println("Mail has been sent successfully");
+                        } catch (MessagingException mex) {
+                            System.out.println("Unable to send an email" + mex);
+                        }
                     }
                 }
             } catch  (Exception e) {
---
+++ src/com/jannemec/tools/Tools.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jannemec.tools;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ *
+ * @author u935
+ */
+public class Tools {
+    
+    final static public boolean isJar(Class cl) {
+        final File jarFile = new File(cl.getProtectionDomain().getCodeSource().getLocation().getPath());
+        return(jarFile.isFile());
+    }
+    
+    public String getJarPath() throws UnsupportedEncodingException {
+        File currentDir = new File("");
+        //System.out.println("Working Directory : " + currentDir.getAbsoluteFile());
+        return(currentDir.getAbsoluteFile().getAbsolutePath());
+    }
+    
+    /*protected String a() {
+        String path = ClassLoader.getSystemClassLoader().getResource(".").getPath();
+        path =  (new File(path)).getAbsolutePath();
+        String decodedPath = URLDecoder.decode(path, "UTF-8");
+        return(decodedPath);
+    }*/
+    
+    public static InputStream getPropertyFile(Class cl, String path) throws IOException {
+        final File jarFile = new File(cl.getProtectionDomain().getCodeSource().getLocation().getPath());
+        InputStream in = null;
+        if(jarFile.isFile()) {  // Run with JAR file
+            URL url = ClassLoader.getSystemResource(path);
+            if (url != null) {
+                in = url.openStream();
+                //in = cl.getClassLoader().getResourceAsStream(path);
+            }
+        }
+        if (in == null) { // not found or IDE
+            in = new FileInputStream(cl.getResource("/").getPath() + path);  
+        }
+        return(in);
+    }
+
+    Connection conn = null;
+    public void dbfConnect(String connectionString) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        // create a connection to the database
+        Class.forName("org.sqlite.JDBC");
+        //Class.forName("SQLite.JDBCDriver").newInstance();
+        this.conn = DriverManager.getConnection(connectionString);
+        System.out.println("Connection to SQLite has been established.");
+    }
+    
+    public void dbfCreateTable() throws SQLException {
+        if (this.conn != null) {
+            String sqlCreate = "CREATE TABLE IF NOT EXISTS datalog"
+            + "  (id              INTEGER PRIMARY KEY AUTOINCREMENT  NOT NULL,"
+            + "   dttm            DATETIME                           NOT NULL,"
+            + "   name            VARCHAR(15)                        NOT NULL,"
+            + "   valdouble       REAL,"
+            + "   valstring       VARCHAR(20),"
+            + "   valint          INT,"
+            + "   valother        TEXT)";
+
+            Statement stmt = this.conn.createStatement();
+            stmt.execute(sqlCreate);
+        } else {
+            throw(new SQLException("Connection not estabilished"));
+        }
+    }
+    
+    public void storeStringValue(Date dt, String name, String val) throws SQLException {
+        Statement stmt = this.conn.createStatement();
+        String sql = "INSERT INTO datalog (dttm, name, valstring) " +
+                        "VALUES ('" + Tools.getFormatter().format(dt) + "', '" + name + "', '" + val +"');"; 
+        stmt.executeUpdate(sql);
+    }
+    
+    public void storeIntValue(Date dt, String name, int val) throws SQLException {
+        Statement stmt = this.conn.createStatement();
+        String sql = "INSERT INTO datalog (dttm, name, valint) " +
+                        "VALUES ('" + Tools.getFormatter().format(dt) + "', '" + name + "', " + Integer.toString(val) +");"; 
+        stmt.executeUpdate(sql);
+    }
+    
+    public void storeDoubleValue(Date dt, String name, double val) throws SQLException {
+        Statement stmt = this.conn.createStatement();
+        String sql = "INSERT INTO datalog (dttm, name, valdouble) " +
+                        "VALUES ('" + Tools.getFormatter().format(dt) + "', '" + name + "', " + Double.toString(val) +");"; 
+        stmt.executeUpdate(sql);
+    }
+    
+    static protected SimpleDateFormat formatSQL = null;
+    
+    protected static SimpleDateFormat getFormatter() {
+        if (Tools.formatSQL == null) {
+            Tools.formatSQL = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        }
+        return(Tools.formatSQL);
+    }
+}
